0c7f14701217d2c3a316ad20163ae7a0171f0886
Shawn Mikelson <smikelson75@gmail.com>
2025-08-03 23:28:18 +0000

    updated to private module

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "= 3.43.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 3.43.0"
     }
-    cloud {
-        organization = "acloudguru-learn"
+  }
+  cloud {
+    organization = "acloudguru-learn"
 
-        workspaces {
-            name = "Hands-On_with_Terraform_on_Azure"
-        }
-    }  
+    workspaces {
+      name = "Hands-On_with_Terraform_on_Azure"
+    }
+  }
 }
 
 provider "azurerm" {
-    features {}
-    skip_provider_registration = true
+  features {}
+  skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "813-5c7f0903-hands-on-with-terraform-on-azure"
-    location = "East US"
+  name     = "813-5c7f0903-hands-on-with-terraform-on-azure"
+  location = "East US"
 }
 
 module "securestorage" {
-  source               = "app.terraform.io/ps-wayne-hoggett/securestorage/azurerm"
-  version              = "1.0.0"
+  source               = "app.terraform.io/acloudguru-learn/securestorage/azurerm"
+  version              = "1.0.1"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = "stlearnwithacloudgurua0cd3"
